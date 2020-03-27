26aa90fb715485bcf09d5390135b2ec422201197
lirakuma115 <kapibara115@gmail.com>
2020-03-27 09:37:17 +0900

    解答見本

--- C001-radio.php
+++ C001-radio.php
@@ -1,33 +1,81 @@
+<?php
+ if(isset($_GET['submit'])){
+   //採点ボタンを押していたら
+   $seikai = 0;
+   $trueAns = [0,5,8,10,14];
+   //foreach($trueAns as $value){
+     //正解の配列を直接回せる
+     //if($_GET['Q1'] == $value)
+     for($i=0; $i<5; $i++) {
+       $q = $i+1 ;//Qは1から始まるので
+       if(isset($_GET["Q$q"])&&
+       $_GET["Q$q"] == $trueAns[$i])
+     $seikai++;
+   }
+   echo "あなたは",$seikai*20,"点でした！";
+ }
+?>
 <form name="radioB">
-カナダの首都は？<br>
-<input type="radio" name="Q1">オタワ<br>
-<input type="radio" name="Q1">トロント<br>
-<input type="radio" name="Q1">モントリオール<br>
+<?php
+$city = ["オタワ",
+"トロント",
+"モントリオール",
+"ジュネーブ",
+"チューリッヒ",
+"ベルン",
+"ハンブルク",
+"ブレーメン",
+"ベルリン",
+"バルセロナ",
+"マドリード",
+"リスボン",
+"シドニー",
+"メルボルン",
+"キャンベラ"];
+$country = ["カナダ",
+"スイス",
+"ドイツ",
+"スペイン",
+"オーストラリア"];
+//この配列をループして問題を作りたい。3回に1回は問題番号が変わる。国名も変わる。余剰演算子を使う
+$c = 0;
+foreach($city as $key => $value){
+  if($key % 3 == 0)
+  echo "<br>" ,$country[$c++], "の首都は？<br>";
+  echo "<input type='radio' name='Q$c' value='$key'> $value <br>";
+}
+?>
+
+<!-- カナダの首都は？<br> 
+<input type="radio" name="Q1" value="0">オタワ<br>
+<input type="radio" name="Q1" value="1">トロント<br>
+<input type="radio" name="Q1" value="2">モントリオール<br>
 <br>
 スイスの首都は？<br>
-<input type="radio" name="Q2">ジュネーブ<br>
-<input type="radio" name="Q2">チューリッヒ<br>
-<input type="radio" name="Q2">ベルン<br>
+<input type="radio" name="Q2" value="3">ジュネーブ<br>
+<input type="radio" name="Q2" value="4">チューリッヒ<br>
+<input type="radio" name="Q2" value="5">ベルン<br>
 <br>
 ドイツの首都は？<br>
-<input type="radio" name="Q3">ハンブルク<br>
-<input type="radio" name="Q3">ブレーメン<br>
-<input type="radio" name="Q3">ベルリン<br>
+<input type="radio" name="Q3" value="6">ハンブルク<br>
+<input type="radio" name="Q3" value="7">ブレーメン<br>
+<input type="radio" name="Q3" value="8">ベルリン<br>
 <br>
 スペインの首都は？<br>
-<input type="radio" name="Q4">バルセロナ<br>
-<input type="radio" name="Q4">マドリード<br>
-<input type="radio" name="Q4">リスボン<br>
+<input type="radio" name="Q4" value="9">バルセロナ<br>
+<input type="radio" name="Q4" value="10">マドリード<br>
+<input type="radio" name="Q4" value="11">リスボン<br>
 <br>
 オーストラリアの首都は？<br>
-<input type="radio" name="Q5">シドニー<br>
-<input type="radio" name="Q5">メルボルン<br>
-<input type="radio" name="Q5">キャンベラ<br>
-<br>
+<input type="radio" name="Q5" value="12">シドニー<br>
+<input type="radio" name="Q5" value="13">メルボルン<br>
+<input type="radio" name="Q5" value="14">キャンベラ<br>
+<br>-->
 <input type="button" value="採点" onclick="saiten()" />
+
+ <input type="submit" name="submit" value="php採点">  <!--phpでやる採点 -->
 </form>
-
-
+ 
 
 <script>
 function saiten(){
